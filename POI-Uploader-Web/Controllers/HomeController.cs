227c01b5e5b14ecb478bd4dd96c18edc83f3f086
Henry Pengye Xia <xiapengye@gmail.com>
2013-04-04 23:43:34 -0500

    Adding multi-threading to uploader

--- POI-Uploader-Web/Controllers/HomeController.cs
+++ POI-Uploader-Web/Controllers/HomeController.cs
@@ -7,19 +7,18 @@ using System.Web.Mvc;
 using System.IO;
 using POILibCommunication;
 using Communication;
+using System.Threading;
 
 namespace POI_Uploader_Web.Controllers
 {
     public class HomeController : Controller
     {
-
+        
         public ActionResult UploadPresentation()
         {
             String presentor = "unknown";
             String description = "unknown";
             
-            
-
             if (Request.Form.AllKeys.Contains("presentor"))
             {
                 presentor = Request.Form["presentor"];
@@ -44,27 +43,45 @@ namespace POI_Uploader_Web.Controllers
 
                 hpf.SaveAs(savedFn);
 
-                switch(Path.GetExtension(presFn))
-                {
-                    case @".PDF":
-                    case @".pdf":
-                        POIPDFProcessor.Process(savedFn, presentor, description);
-                        break;
-                    case @".PPT":
-                    case @".ppt":
-                    case @".PPTX":
-                    case @".pptx":
-                        POIPPTProcessor.Process(savedFn, presentor, description);
-                        break;
-                    case @".POI":
-                        POIFileReader reader = new POIFileReader(savedFn);
-                        reader.GetImageAndAnimationFromFile();
-                        break;
-                }
+                //Create a new thread and start handling
+                String[] param = new String[4];
+                param[0] = Path.GetExtension(presFn);
+                param[1] = savedFn;
+                param[2] = presentor;
+                param[3] = description;
 
+                Thread fileHandler = new Thread(HandleUploadedFile);
+                fileHandler.Start(param);
             }
 
             return Json("Presentation files processed!");
+        }
+
+        private void HandleUploadedFile(object arg)
+        {
+            String[] param = arg as String[];
+            String extName = param[0];
+            String savedFn = param[1];
+            String presentor = param[2];
+            String description = param[3];
+
+            switch (extName)
+            {
+                case @".PDF":
+                case @".pdf":
+                    POIPDFProcessor.Process(savedFn, presentor, description);
+                    break;
+                case @".PPT":
+                case @".ppt":
+                case @".PPTX":
+                case @".pptx":
+                    POIPPTProcessor.Process(savedFn, presentor, description);
+                    break;
+                case @".POI":
+                    POIFileReader reader = new POIFileReader(savedFn);
+                    reader.GetImageAndAnimationFromFile();
+                    break;
+            }
         }
     }
 }
